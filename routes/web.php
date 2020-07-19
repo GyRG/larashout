90915ee648771a69996f0bc5d34139c87f48c22f
GyRG <gyorgyluisr@gmail.com>
2020-07-18 23:38:57 -0500

    web

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['prefix' => 'admin'], function() {
 
     Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
     Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
-    Rpute::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
+    Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
 
     Route::get('/', function () {
         return view('admin.dashboard.index');
