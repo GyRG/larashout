63c45449bb46e8a15fd269f0865cc9a2a4a8a0c7
GyRG <gyorgyluisr@gmail.com>
2020-07-18 22:31:09 -0500

    Agregar rutas de admin

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+require 'admin.php';
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/admin', 'admin.dashboard.index');
+Route::group(['prefix' => 'admin'], function() {
+
+    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
+    Rpute::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
+
+    Route::get('/', function () {
+        return view('admin.dashboard.index');
+    });
+});
 
 
 
