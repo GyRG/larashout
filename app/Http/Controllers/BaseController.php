884189a9bbff9271fc677c39dc46af27d5b1c99b
GyRG <gyorgyluisr@gmail.com>
2020-08-07 20:51:33 -0500

    Se se agrega al BaseController una función que envía una respuesta json al

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -31,4 +31,23 @@ protected function showErrorPage($errorCode = 404, $message = null)
     $data['message'] = $message;
     return response()->view('errors.'.$errorCode, $data, $errorCode);
 }
+/**
+ * @param bool $error
+ * @param int $responseCode
+ * @param array $message
+ * @param null $data
+ * @return \Illuminate\Http\JsonResponse
+ */
+protected function responseJson($error = true, $responseCode = 200, $message = [], $data = null)
+{
+    return response()->json([
+        'error'         =>  $error,
+        'response_code' => $responseCode,
+        'message'       => $message,
+        'data'          =>  $data
+    ]);
+}
+
+
+
 }
