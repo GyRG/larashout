c9640f2dc64e15de693e794b12add28225d6e8d4
GyRG <gyorgyluisr@gmail.com>
2020-08-09 16:22:04 -0500

    Registra el settingServiceProvider  en app.php correctamente

--- config/app.php
+++ config/app.php
@@ -146,7 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Providers\SettingServiceProvider::class,
+        //Illuminate\Providers\SettingServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -160,6 +160,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\SettingServiceProvider::class,
 
     ],
 
