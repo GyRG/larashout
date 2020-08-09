7c49f931d5fe10dac960bf40187dc9c6aba21c33
GyRG <gyorgyluisr@gmail.com>
2020-08-09 16:24:34 -0500

    Actualiza la función update De este modo se utiliza el trato UploadAble  también se agrega el modelo Setting

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Setting;
 use App\Traits\UploadAble;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
+//use FlashMessages;
 
 class SettingController extends BaseController
 {
     use UploadAble;
+    
     /**
  * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
  */
@@ -20,9 +23,35 @@ public function index()
 
 /**
  * @param Request $request
+ * @return \Illuminate\Http\RedirectResponse
  */
 public function update(Request $request)
 {
+    if ($request->has('site_logo') && ($request->file('site_logo') instanceof UploadedFile)) {
 
+        if (config('settings.site_logo') != null) {
+            $this->deleteOne(config('settings.site_logo'));
+        }
+        $logo = $this->uploadOne($request->file('site_logo'), 'img');
+        Setting::set('site_logo', $logo);
+
+    } elseif ($request->has('site_favicon') && ($request->file('site_favicon') instanceof UploadedFile)) {
+
+        if (config('settings.site_favicon') != null) {
+            $this->deleteOne(config('settings.site_favicon'));
+        }
+        $favicon = $this->uploadOne($request->file('site_favicon'), 'img');
+        Setting::set('site_favicon', $favicon);
+
+    } else {
+
+        $keys = $request->except('_token');
+
+        foreach ($keys as $key => $value)
+        {
+            Setting::set($key, $value);
+        }
+    }
+    return $this->responseRedirectBack('Settings updated successfully.', 'success');
 }
 }
