b124afa73a02af3b5e2b6ff2260c52b07072cc4a
GyRG <gyorgyluisr@gmail.com>
2020-08-08 19:52:15 -0500

    registra correctamente el SettingServiceProvider en app.php

--- config/app.php
+++ config/app.php
@@ -146,7 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Support\ServiceProvider::class,
+        Illuminate\Providers\SettingServiceProvider::class,
 
         /*
          * Package Service Providers...
