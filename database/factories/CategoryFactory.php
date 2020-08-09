29613f41098cba569617ef2cb56d6a531cd5dab4
GyRG <gyorgyluisr@gmail.com>
2020-08-09 17:42:27 -0500

    Codifica CategoriesTableSeeder Category Factory y se llama al Seeder desde el DatabaseSeeder

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Models\Category;
 use Faker\Generator as Faker;
 
-$factory->define(App\Models\Category::class, function (Faker $faker) {
+$factory->define(Category::class, function (Faker $faker) {
     return [
-        //
+        'name'          =>  $faker->name,
+        'description'   =>  $faker->realText(100),
+        'parent_id'     =>  1,
+        'menu'          =>  1,
     ];
 });
